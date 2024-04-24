6d9f7b38ab8eb48e813588439abc936fd21fc765
adan_due <ing.adanduenas@gmail.com>
2024-04-24 11:47:39 -0600

    Estructura inicial del Header

---
+++ grupoandal-react/src/Components/App/index.js
@@ -0,0 +1,13 @@
+import { Header } from '../Header';
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <Header />
+    <h1>Andal Minerales</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ grupoandal-react/src/Components/Header/index.js
@@ -0,0 +1,32 @@
+
+
+const Header = () => {
+    return (
+        <>
+        <header className="header">
+        
+        <nav class="menu">
+            <ul>
+                <li>
+                    <a href="#gypsum" className="link">Gypsum</a>
+                </li>
+                <li>
+                    <a href="#piedraYeso" className="link">Piedra de Yeso</a>
+                </li>
+                <li>
+                    <a href="#yesoAgricola" className="link">Yeso Agrícola</a>
+                </li>
+                <li>
+                    <a href="#yesoEspecial" className="link">Yesos Especiales</a>
+                </li>
+                <li>
+                    <a href="#contacto" className="link">Contacto</a>
+                </li>
+            </ul>
+        </nav>
+        </header>
+        </>
+    )
+}
+
+export { Header }
